b4078999af5db36567e898c23481620d879a43df
Mark <market4my@google.com>
2026-03-11 21:08:37 +0300

    Gallery 800x600

--- script.js
+++
@@ -1,9 +0,0 @@
-
-setTimeout(() => {
-  let parent = document.querySelector('#parent');
-  const p = document.createElement("p");
-  let elem = parent.querySelector('.text');
-  p.innerText = "viglina@gmail.com";
-  parent.appendChild(p);
-}, 10000);
-
